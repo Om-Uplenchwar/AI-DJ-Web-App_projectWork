a1e9b840b0beff091de09900237a5341c6ad47b7
Om-Uplenchwar <81371929+Om-Uplenchwar@users.noreply.github.com>
2022-10-22 14:09:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ song2 = "";
 
 function preload() {
     song1 = loadSound("music.mp3");
-    song2 =loadSound("music2.mp3");
+    song2 = loadSound("music2.mp3");
 }
 
 function setup() {
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(600, 400);
     canvas.center();
 
     video = createCapture(VIDEO);
