17d5b0857bddeff193ef3f99f56173a13d6e76e3
Om-Uplenchwar <81371929+Om-Uplenchwar@users.noreply.github.com>
2022-10-29 13:45:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -12,6 +16,13 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initialized');
 }
 
 function draw() {
@@ -20,4 +31,20 @@ function draw() {
 
 function play() {
     song.play("music.mp3");
+    song.setVolume(1);
+    song.rete(1);
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = " + rightWristY);
+    }
 }
