8d479321c060fbe049413627f2bbc3d0819fe80b
Om-Uplenchwar <81371929+Om-Uplenchwar@users.noreply.github.com>
2022-11-05 14:11:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+song1_status = "";
+song2_status = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -27,6 +32,34 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    fill("#FF0000");
+    stroke("FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+
+        if(song1_status == false)
+        {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "currantly playing = Harry Potter Theme";
+        }
+    }
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(RightWristX, RightWristY, 20);
+        song1.stop();
+
+        if(song2_status == false)
+        {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "currantly playing = Peter Pan Theme";
+        }
+    }
 }
 
 function play() {
@@ -39,6 +72,12 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " + scoreRightWrist);
+
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = " + leftWristY);
